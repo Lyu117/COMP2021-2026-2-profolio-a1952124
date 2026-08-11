2bb0e0125db18510c963818383f99727d7215e0f
Lyu117 <1428218221@qq.com>
2026-08-11 13:13:25 +0930

    Payroll class and constructor validation

---
+++ Week2/PracA/Task1/Payroll.cs
@@ -0,0 +1,70 @@
+﻿namespace Payroll
+{
+    class Payroll
+    {
+       private double _hours;
+       private decimal _rate;
+
+       private decimal _taxRate;
+
+       public Payroll(double hours, decimal rate,decimal taxRate)
+        {
+           
+
+            if (hours < 0)
+
+            {
+            throw new ArgumentException("Hours cannot be negative.");
+            }
+
+            if (rate < 0)
+            {
+                throw new ArgumentException("rates cannot be negative.");
+            }
+
+            if (taxRate < 0)
+            {
+                throw new ArgumentException("taxRate can not be negative.");
+            }
+
+
+             _hours=hours;
+
+            _rate=rate;
+
+            _taxRate=taxRate;
+
+
+
+
+        }
+
+
+        public decimal CalculateNetPay()
+        {
+             decimal grossPay = (decimal)_hours * _rate;
+
+             decimal tax = grossPay * _taxRate;
+
+             decimal netPay = grossPay - tax;
+
+    return netPay;
+        }
+
+        
+
+       
+
+
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+             Payroll p1 = new Payroll(40,25m,0.2m);
+
+
+        }
+    }
+}
