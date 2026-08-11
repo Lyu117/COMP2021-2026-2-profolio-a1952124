4b136466b725ea07aedc9ae2a8c2d2c465145658
Lyu117 <1428218221@qq.com>
2026-08-11 13:46:21 +0930

    change to auto-properties

---
+++ Week2/PracA/Task2/Payroll.cs
@@ -0,0 +1,123 @@
+﻿namespace Payroll
+{
+    class Payroll
+    {
+
+        private double _hours;
+        private decimal _rate;
+        private decimal _taxRate;
+
+
+       // hours Property
+        public double Hours
+        {
+            get
+            {
+                return _hours;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Hours can not be negative.");
+
+                }
+                _hours=value;
+            }
+        }
+        // rate property
+        public decimal Rate
+        {
+            get
+            {
+                return _rate;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Rate cannot be negative.");
+                }
+
+                _rate = value;
+            }
+        }
+
+
+        // TaxRate Property
+        public decimal TaxRate
+        {
+            get
+            {
+                return _taxRate;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("TaxRate cannot be negative.");
+                }
+
+                _taxRate = value;
+            }
+        }
+       public Payroll(double hours, decimal rate,decimal taxRate)
+        {
+           
+
+            Hours = hours;
+            Rate = rate;
+            TaxRate = taxRate;
+        }
+
+        //Method of CaculateNetPay
+        public decimal CalculateNetPay()
+        {
+             decimal grossPay = (decimal)_hours * _rate;
+
+             decimal tax = grossPay * _taxRate;
+
+             decimal netPay = grossPay - tax;
+
+             return netPay;
+
+        }
+
+
+        //Method of change tax rate
+
+        public void ChangeTaxRate(
+            decimal newTaxRate)
+        { 
+            if (newTaxRate < 0)
+            {
+                throw new ArgumentException("The newTaxRate can not be negative.");
+
+
+            }
+
+            _taxRate=newTaxRate;
+            
+        }
+
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+             Payroll p1 = new Payroll(40,25m,0.2m);
+             Console.WriteLine(p1.CalculateNetPay());
+
+
+            p1.ChangeTaxRate(0.25m);
+
+             Console.WriteLine(p1.CalculateNetPay());
+
+
+        }
+    }
+}
