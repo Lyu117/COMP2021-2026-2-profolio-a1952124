5375266f47e20737f5e9d77bf8636d7409f52f48
Lyu117 <1428218221@qq.com>
2026-08-13 14:29:52 +0930

    Start

---
+++ Week2/PracB/Task1/Bank(Task1).cs
@@ -0,0 +1,31 @@
+﻿using System.Buffers;
+using System.Dynamic;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        public string Owner {get;set;}
+        public decimal Balance {get;set;}
+
+        public BankAccount(string owner, decimal blance)
+        {
+            if (blance <= 0)
+            {
+                throw new ArgumentException("Empty balance. ");
+
+            }
+
+            Owner=owner;
+            Balance=blance;
+            
+        }
+       public decimal Deposit(decimal amount)
+        {
+            
+        }
+
+        
+
+    }
+}
