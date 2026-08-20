a9d1361582a0d6c156a7e68344bb81eab2d7224b
Lyu117 <1428218221@qq.com>
2026-08-20 15:16:51 +0930

    Finish Task2

--- Week3/PracB/Task2/Program.cs
+++ Week3/PracB/Task2/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EmployeeSystem;
+
+class Programs{
+
+public static void Main(){
+List<Employee> employees =new List<Employee>();
+
+employees.Add(new FullTimeEmployee("Bob", 6250m));
+employees.Add(new Contractor("Fred", 25m, 100m));
+
+foreach (Employee employee in employees)
+{
+    decimal netPay = employee.CalculatePay();
+    decimal tax = netPay / (1 - Employee.TaxRate) * Employee.TaxRate;
+
+    Console.WriteLine($"{employee.Name}: Pay ${netPay}. Tax ${tax}.");
+}
+}
+}
