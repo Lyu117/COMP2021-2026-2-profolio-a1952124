0b8adbb05fa02ebad8ae9e8ce64d794f8beed822
Lyu117 <1428218221@qq.com>
2026-08-20 14:45:11 +0930

    Update Program.cs

--- Week3/PracB/Task1/Program.cs
+++ Week3/PracB/Task1/Program.cs
@@ -1,4 +1,9 @@
 ﻿namespace EmployeeSystem;
+
+interface IReportable
+{
+    string GenerateReport();
+}
 abstract public class Employee
 {
     public string Name{get;set;}
@@ -7,19 +12,38 @@ abstract public class Employee
         Name=name;
     }
     public const decimal TaxRate=0.2m;
+    
 }
 
-public class FullTimeEmployee : Employee
+public class FullTimeEmployee : Employee, IReportable
 {
-    public FullTimeEmployee(string name) : base(name)
-    {
-        Name=name;
-    }
     public decimal AnnualSalary{get;set;}
+    public FullTimeEmployee(string name,decimal annualSalary) : base(name)
+    {
+       AnnualSalary=annualSalary;
+    }
+
+    public decimal CalculatePay()
+    {
+        decimal Tax=AnnualSalary*TaxRate;
+        
+        return AnnualSalary-Tax;
+    }
+    public string GenerateReport()
+    {
+        return $"Name:{Name}, AnnualSalary:{AnnualSalary}, PayTax:{CalculatePay()}";
+    }
 
 }
 
 public class Contractor : Employee
 {
-    public Contractor(string name):
+    public decimal Rate{get;set;}
+    public decimal Hours{get;set;}
+
+    public Contractor(string name,decimal rate,decimal hours) : base(name)
+    {
+        Rate=rate;
+        Hours=hours;
+    }
 }
