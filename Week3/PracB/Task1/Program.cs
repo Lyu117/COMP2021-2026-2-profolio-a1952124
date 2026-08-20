6822c5fb88a6f743aaac349762720d1cbd8d6a3b
Lyu117 <1428218221@qq.com>
2026-08-20 14:24:28 +0930

    Start WeekPracB Task1

---
+++ Week3/PracB/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeSystem;
+abstract public class Employee
+{
+    public string Name{get;set;}
+    public Employee(string name)
+    {
+        Name=name;
+    }
+    public const decimal TaxRate=0.2m;
+}
+
+public class FullTimeEmployee : Employee
+{
+    public FullTimeEmployee(string name) : base(name)
+    {
+        Name=name;
+    }
+    public decimal AnnualSalary{get;set;}
+
+}
+
+public class Contractor : Employee
+{
+    public Contractor(string name):
+}
