4650e2686d2c99058e04364c3d3bba4cc853dfc2
Lyu117 <1428218221@qq.com>
2026-08-25 14:00:05 +0930

    Finish
    Finish

--- Week4/PracA/BrokenProgram.cs
+++ Week4/PracA/BrokenProgram.cs
@@ -1,34 +1,36 @@
-﻿namespace Topic4a;
-public static class BrokenProgram
+﻿using System.Globalization;
+
+namespace Topic4a;
+
+internal static class BrokenProgram
 {
-  static void Main(string[] args)
+  private static void Main()
   {
     const string input = """
-       23
-       + 
-       77
-       + 
+        23
+        +
+        77
+        +
         3
-       + 
-      457
-       * 
+        +
+        457
+        *
         2
-         
-      """;
+        """;
     int total = 0;
     List<int> items = [];
     List<string> symbols = [];
+    string[] lines = input.Split('\n');
 
-    for (int i = 0; i < input.Split("\n").Length; i += 2)
+    for (int i = 0; i < lines.Length; i += 2)
     {
-      items.Add(int.Parse(input.Split("\n")[i]));
+      items.Add(int.Parse(lines[i], CultureInfo.InvariantCulture));
 
       if (i != 0)
       {
-        symbols.Add(input.Split("\n")[i - 1].Trim());
+        symbols.Add(lines[i - 1].Trim());
       }
     }
-
     foreach (var (i, value) in items.Index())
     {
       if (i == 0) { total = value; }
@@ -41,7 +43,8 @@ public static class BrokenProgram
         total += value;
       }
     }
-    Console.WriteLine("Total was: " + total);
+    Console.WriteLine(
+        string.Create(CultureInfo.InvariantCulture, $"Total was: {total}"));
     Console.WriteLine("Expected total was: 1120");
     Console.WriteLine(total == 1120 ? "Your answer was RIGHT!" : "Your answer was WRONG!\nGo and fix it.");
   }
