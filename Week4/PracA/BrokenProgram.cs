20512cbfdcfe51fe417ee2d982d265305e4a9913
Lyu117 <1428218221@qq.com>
2026-08-25 12:12:28 +0930

    Strat with Week4 Task1

---
+++ Week4/PracA/BrokenProgram.cs
@@ -0,0 +1,49 @@
+﻿namespace Topic4a;
+public static class BrokenProgram
+{
+  static void Main(string[] args)
+  {
+    const string input = """
+       23
+       + 
+       77
+       + 
+        3
+       + 
+      457
+       * 
+        2
+         
+      """;
+    int total = 0;
+    List<int> items = [];
+    List<string> symbols = [];
+
+    for (int i = 0; i < input.Split("\n").Length; i += 2)// input.Split（"\n") 把input按换行切开 
+                                                         // 切开后一共有多少行就是input.Split("\n").Length
+    {
+      items.Add(int.Parse(input.Substring((i * 4) + 1, 3)));
+
+      if (i != 0)
+      {
+        symbols.Add(input.Split("\n")[i - 1]);
+      }
+    }
+
+    foreach (var (i, value) in items.Index())
+    {
+      if (i == 0) { total = value; }
+      else if (symbols[i - 1] == "*")
+      {
+        total *= value;
+      }
+      else
+      {
+        total += value;
+      }
+    }
+    Console.WriteLine("Total was: " + total);
+    Console.WriteLine("Expected total was: 1120");
+    Console.WriteLine(total == 1120 ? "Your answer was RIGHT!" : "Your answer was WRONG!\nGo and fix it.");
+  }
+}
