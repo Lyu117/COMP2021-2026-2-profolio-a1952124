b0fb284a6d62116f55c40516a43947606a9f1839
Lyu117 <1428218221@qq.com>
2026-09-01 12:32:53 +0930

    Start doing week5 pracA

---
+++ Week5/PracA/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ArraysPrac
+{
+    class Progress
+    {
+        static void Main()
+        {
+            string[]names=new string[10];
+            names[0]="David";
+            names[1]="Judy";
+            names[2]="Lucy";
+            names[3]="Tom";
+            names[4]="Rebeacca";
+            names[5]="Bob";
+            names[6]="V";
+            names[7]="Max";
+            names[8]="Lucia";
+            names[9]="Martin";
+
+
+            foreach(string name in names)
+            {
+                System.Console.WriteLine(name);
+            }
+
+            foreach(string name in names)
+
+
+            
+        }
+    }
+}
