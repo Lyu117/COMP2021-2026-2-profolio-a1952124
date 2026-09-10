3315d91ad348417f732071145739560eb40dd7c5
Lyu117 <1428218221@qq.com>
2026-09-10 14:16:40 +0930

    Finish Task1 Start Task2

--- Week6/PracB/Task1/Program.cs
+++ Week6/PracB/Task1/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Task1
+{
+    class Progress
+    {
+        static void Main()
+        {
+            List<string> name = new List<string>();
+            name.Add("Lewis Hamilton");
+            name.Add("Michael Schumacher");
+            name.Add("Max Verstappen");
+            name.Add("Fernando Alonso");
+            name.Add("Sebastian Vettel");
+            name.Add("Ayrton Senna");
+            name.Add("Alain Prost");
+            name.Add("Charles Leclerc");
+            name.Add("Lando Norris");
+            name.Add("Kimi Räikkönen");
+            var Sortedname=name.OrderBy(x=>x).ToList();
+            System.Console.WriteLine("Print Sorted Name: ");
+            foreach(string name1 in Sortedname)
+            {
+                System.Console.WriteLine(name1);
+            }
+            System.Console.WriteLine();
+
+
+        }
+    }
+}
