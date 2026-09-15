b7baa980c63509a8c818b28b23bab9c81a4ca999
Lyu117 <1428218221@qq.com>
2026-09-15 12:19:54 +0930

    Update Program.cs

--- Week7/PracA/SortedSet/Program.cs
+++ Week7/PracA/SortedSet/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿namespace Sort
+{
+    class SortPrac
+    {
+        void DisplayScores(SortedSet<int> scores)
+        {
+            System.Console.WriteLine("SortedSet is ");
+            foreach(int score in scores)
+            {
+                System.Console.WriteLine(score);
+            }
+        }
+    }
+
+
+    class Progress
+    {
+        static void Main()
+        {
+            SortedSet<int>scores=new SortedSet<int>();
+            scores.Add(50);
+            scores.Add(20);
+            scores.Add(80);
+            scores.Add(30);
+            scores.Add(5);
+            scores.Add(45);
+            scores.Add(99);
+            scores.Add(100);
+            scores.Add(6);
+            scores.Add(42);
+        }
+    }
+}
